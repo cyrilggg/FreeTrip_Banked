c1b9b629d24b514b5531b36fd0522598accc54c6
cyrilggg <344327193@qq.com>
2022-06-27 15:53:17 +0800

    initial

---
+++ src/main/java/com/example/freetrip_banked/FreeTripBankedApplication.java
@@ -0,0 +1,13 @@
+package com.example.freetrip_banked;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreeTripBankedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreeTripBankedApplication.class, args);
+    }
+
+}
